744b76bb520fd3fb081291f5f0855e38fb63c031
Roger Oliva <rooliva@microsoft.com>
2025-03-20 21:43:23 -0600

    .

---
+++ Tips And Trick/machinesetgpu.sh
@@ -0,0 +1,24 @@
+
+
+
+apiVersion: machine.openshift.io/v1beta1
+kind: MachineSet
+metadata:
+  name: aro-cluster-abcd1-spot-eastus
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      machine.openshift.io/cluster-api-cluster: aro-cluster-abcd1
+      machine.openshift.io/cluster-api-machineset: aro-cluster-abcd1-spot-eastus
+  template:
+    metadata:
+        machine.openshift.io/cluster-api-machineset: aro-cluster-abcd1-spot-eastus
+    spec:
+      providerSpec:
+        value:
+          spotVMOptions: {}
+      taints:
+        - effect: NoExecute
+          key: spot
+          value: 'true'
